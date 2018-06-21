60e26bb475f0e2f85dbbb68656ff145b8b9fe8b9
Milan <makadi.milan.mate@gmail.com>
2018-06-21 13:42:18 +0200

    asd

---
+++ Assets/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] GameObject firstSelected;
+
+    void Start()
+    {
+        eventSystem.SetSelectedGameObject(firstSelected);
+    }
+
+}
